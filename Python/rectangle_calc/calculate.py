3560327864b7099a60687403f70894bdf4d09fc3
sammvuseni <sammvuseni@gmail.com>
2025-07-07 18:20:39 +0100

    Week_09 Conclusion

---
+++ Python/rectangle_calc/calculate.py
@@ -0,0 +1,7 @@
+def area(length, width):
+   #Function to calculate the area
+   return length * width
+
+def perimeter(length, width):
+   #Function to calculate the perimeter
+   return 2 * (length + width)
